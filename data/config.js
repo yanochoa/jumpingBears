5a705e8ca1148d8bed7a6cfd99ef0a2237b657e7
yanochoa <yanochoa@email.arizona.edu>
2019-02-06 21:54:35 -0700

    change to config file, added twitter

--- data/config.js
+++ data/config.js
@@ -9,6 +9,10 @@ module.exports = {
   siteFBAppID: "", // FB Application ID for using app insights
   googleTagManagerID: "", // GTM tracking ID.
   disqusShortname: "jumping-bears-party-rentals", // Disqus shortname.
+  userName: "Yan Ochoa",
+  userTwitter: "n/a",
+  userLocation: "Phoenix, AZ",
+  userDescription: "",
   copyright:
     "Copyright © Developed by Yan Ochoa 2018 for Jumping Bears Party Rentals. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#00d1b2", // Used for setting manifest and progress theme colors.
